51167f54887ab989247012d24cd2a7603aa3e01b
Tien Dao <tiendao5240@gmail.com>
2022-03-23 20:10:57 -0400

    Add layout and strings table

---
+++ app/src/main/java/tdao/example/notificationandsms_tiendao/MainActivity.kt
@@ -0,0 +1,14 @@
+package tdao.example.notificationandsms_tiendao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onButtonClick(view: View) {}
+}
