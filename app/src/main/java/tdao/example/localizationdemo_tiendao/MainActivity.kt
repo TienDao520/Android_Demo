894b0b749871043d213b3c9e1a48c05c45cb857f
Tien Dao <tiendao5240@gmail.com>
2022-03-29 19:52:26 -0400

    Localization: first commit

---
+++ app/src/main/java/tdao/example/localizationdemo_tiendao/MainActivity.kt
@@ -0,0 +1,11 @@
+package tdao.example.localizationdemo_tiendao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
