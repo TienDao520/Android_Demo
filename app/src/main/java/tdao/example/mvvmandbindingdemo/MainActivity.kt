ae8c7d5d7fa8653b80068c7707cac662381e0209
Tien Dao <tiendao5240@gmail.com>
2022-04-07 10:24:44 -0400

    Check the data get back from viewModel
    - The view knows about the viewModel but doesn't know the model

--- app/src/main/java/tdao/example/mvvmandbindingdemo/MainActivity.kt
+++ app/src/main/java/tdao/example/mvvmandbindingdemo/MainActivity.kt
@@ -2,6 +2,7 @@ package tdao.example.mvvmandbindingdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
@@ -28,5 +29,14 @@ class MainActivity : AppCompatActivity() {
         //store all data in viewModel
         viewModel.count++
         textViewCount.setText(viewModel.count.toString())
+    }
+
+    fun onButtonPerson(view: View) {
+        //Step4: not only it update viewModel but also person instance in MainViewModel
+        viewModel.person.name = viewModel.name
+        viewModel.person.count = viewModel.count
+        Log.i("MainActivity-ViewModel", viewModel.name)
+        Log.i("MainActivity-ViewModel", viewModel.count.toString())
+        Log.i("MainActivity-ViewModel", viewModel.person.toString())
     }
 }
