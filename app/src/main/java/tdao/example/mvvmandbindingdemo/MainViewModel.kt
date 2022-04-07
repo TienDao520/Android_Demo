5dd930309a85564f86a3b5429702d454ed4f9d35
Tien Dao <tiendao5240@gmail.com>
2022-04-07 10:09:58 -0400

    Create a person object in MainViewModel

--- app/src/main/java/tdao/example/mvvmandbindingdemo/MainViewModel.kt
+++ app/src/main/java/tdao/example/mvvmandbindingdemo/MainViewModel.kt
@@ -5,6 +5,10 @@ import androidx.lifecycle.ViewModel
 
 //ViewModel can be reused in many activites
 class MainViewModel: ViewModel() {
+
+    //Step3: Create a person object
+    var person: Person = Person("",0)
+
     // lifecycle
     init {
         Log.i("MainViewModel", "init")
