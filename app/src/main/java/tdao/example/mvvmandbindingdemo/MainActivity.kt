65f98d79df5783525823e9b73b4a7e2a366e2361
Tien Dao <tiendao5240@gmail.com>
2022-04-07 10:06:52 -0400

    Create Model (Person)

--- app/src/main/java/tdao/example/mvvmandbindingdemo/MainActivity.kt
+++ app/src/main/java/tdao/example/mvvmandbindingdemo/MainActivity.kt
@@ -2,10 +2,18 @@ package tdao.example.mvvmandbindingdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Step2: Returns an existing ViewModel or
+        // creates a new one in the scope (usually, a fragment or an activity), associated with this ViewModelProvider
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
 }
---
+++ app/src/main/java/tdao/example/mvvmandbindingdemo/Person.kt
@@ -0,0 +1,4 @@
+package tdao.example.mvvmandbindingdemo
+
+data class Person(var name:String, var count:Int) {
+}
