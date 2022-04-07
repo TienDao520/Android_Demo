bb81ee63a460448383d583334d8d6c97bf2e06aa
Tien Dao <tiendao5240@gmail.com>
2022-04-07 14:45:12 -0400

    Added logs

--- app/src/main/java/tdao/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/tdao/example/gesturedemo/MainActivity.kt
@@ -2,6 +2,7 @@ package tdao.example.gesturedemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 
@@ -12,26 +13,31 @@ class MainActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
     }
 
     override fun onDown(p0: MotionEvent?): Boolean {
-        TODO("Not yet implemented")
+        Log.i("onDown", "onDown: $p0")
+        return true
     }
 
     override fun onShowPress(p0: MotionEvent?) {
-        TODO("Not yet implemented")
+        Log.i("onShowPress", "onShowPress: $p0")
     }
 
     override fun onSingleTapUp(p0: MotionEvent?): Boolean {
-        TODO("Not yet implemented")
+        Log.i("onSingleTapUp", "onSingleTapUp: $p0")
+        return true
     }
 
     override fun onScroll(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
-        TODO("Not yet implemented")
+        Log.i("onScroll", "onScroll: $p0 $p1")
+        return true
     }
 
     override fun onLongPress(p0: MotionEvent?) {
-        TODO("Not yet implemented")
+        Log.i("onLongPress", "onLongPress: $p0")
     }
 
     override fun onFling(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
-        TODO("Not yet implemented")
+        Log.i("onFling", "onFling: $p2 $p3")
+        return true
     }
+
 }
