8c56190afca6f6df90594748bc1190d6c4d27da8
Tien Dao <tiendao5240@gmail.com>
2022-04-08 20:54:51 -0400

    Declare viewModel in MainActivity

--- app/src/main/java/tdao/example/project2_memorygame_tiendao/MainActivity.kt
+++ app/src/main/java/tdao/example/project2_memorygame_tiendao/MainActivity.kt
@@ -2,11 +2,20 @@ package tdao.example.project2_memorygame_tiendao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //Returns an existing ViewModel or
+        // creates a new one in the scope (usually, a fragment or an activity), associated with this ViewModelProvider
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
+
+
 
 }
