4fc8d297d456b5b9fc41e8091a2b0b92524ae3dc
Tien Dao <tiendao5240@gmail.com>
2022-04-09 08:38:16 -0400

    Binding viewModel

--- app/src/main/java/tdao/example/mvvmandbindingdemo/MainActivity.kt
+++ app/src/main/java/tdao/example/mvvmandbindingdemo/MainActivity.kt
@@ -6,15 +6,30 @@ import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import tdao.example.mvvmandbindingdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // variable used to show databinding
+    public lateinit var firstname:String
 
     private lateinit var viewModel: MainViewModel
 
+    // setup the binding variable
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // not using the old setContentView, need to use the binding version
+//        setContentView(R.layout.activity_main)
+
+        //DataBinding:Step3: add binding
+        binding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = MainViewModel()
+        binding.executePendingBindings()
+        binding.lifecycleOwner = this
 
         //Step2: Returns an existing ViewModel or
         // creates a new one in the scope (usually, a fragment or an activity), associated with this ViewModelProvider
@@ -23,18 +38,21 @@ class MainActivity : AppCompatActivity() {
 
     fun onButtonUseName(view: View) {
         viewModel.name = findViewById<EditText>(R.id.editTextTextPersonName).text.toString()
+        binding.firstname = findViewById<EditText>(R.id.editTextTextPersonName).text.toString()
+
     }
     fun onButtonCountPress(view: View) {
         var textViewCount: TextView = findViewById<TextView>(R.id.textViewPresses)
         //store all data in viewModel
-        viewModel.count++
+//        viewModel.count++
         textViewCount.setText(viewModel.count.toString())
     }
 
     fun onButtonPerson(view: View) {
         //Step4: not only it update viewModel but also person instance in MainViewModel
         viewModel.person.name = viewModel.name
-        viewModel.person.count = viewModel.count
+        //No need below since now viewModel update directly using binding data
+//        viewModel.person.count = viewModel.count
         Log.i("MainActivity-ViewModel", viewModel.name)
         Log.i("MainActivity-ViewModel", viewModel.count.toString())
         Log.i("MainActivity-ViewModel", viewModel.person.toString())
--- app/src/main/java/tdao/example/mvvmandbindingdemo/MainViewModel.kt
+++ app/src/main/java/tdao/example/mvvmandbindingdemo/MainViewModel.kt
@@ -1,6 +1,8 @@
 package tdao.example.mvvmandbindingdemo
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 //ViewModel can be reused in many activites
@@ -10,7 +12,10 @@ class MainViewModel: ViewModel() {
     var person: Person = Person("",0)
 
     public var name:String = ""
-    public var count:Int = 0
+
+    //DataBinding:Step4: Step up for live changed data
+    private val count_ = MutableLiveData(0)
+    val count: LiveData<Int> = count_
     // lifecycle
     init {
         Log.i("MainViewModel", "init")
@@ -19,5 +24,12 @@ class MainViewModel: ViewModel() {
     override fun onCleared() {
         super.onCleared()
         Log.i("MainViewModel", "onCleared")
+    }
+
+    //DataBinding:Step5: Declare lamda function
+    fun onIncrementCount() {
+        //Since it is multable data it need default value
+        count_.value = (count_.value ?: 0)+1
+        Log.i("ViewModel", "count " +count.toString())
     }
 }
