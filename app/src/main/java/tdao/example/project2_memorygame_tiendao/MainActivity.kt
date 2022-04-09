1c6a2988c99f3322fb972e2180daed1197bc74d5
Tien Dao <tiendao5240@gmail.com>
2022-04-08 23:16:58 -0400

    Game complete without data binding

--- app/src/main/java/tdao/example/project2_memorygame_tiendao/MainActivity.kt
+++ app/src/main/java/tdao/example/project2_memorygame_tiendao/MainActivity.kt
@@ -2,11 +2,24 @@ package tdao.example.project2_memorygame_tiendao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
 import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var buttons: List<ImageButton>
+    private lateinit var imageButton1: ImageButton
+    private lateinit var imageButton2: ImageButton
+    private lateinit var imageButton3: ImageButton
+    private lateinit var imageButton4: ImageButton
+    private lateinit var imageButton5: ImageButton
+    private lateinit var imageButton6: ImageButton
+    private lateinit var imageButton7: ImageButton
+    private lateinit var imageButton8: ImageButton
+
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +28,47 @@ class MainActivity : AppCompatActivity() {
         // creates a new one in the scope (usually, a fragment or an activity), associated with this ViewModelProvider
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
 
+        imageButton1=findViewById(R.id.imageButton1)
+        imageButton2=findViewById(R.id.imageButton2)
+        imageButton3=findViewById(R.id.imageButton3)
+        imageButton4=findViewById(R.id.imageButton4)
+        imageButton5=findViewById(R.id.imageButton5)
+        imageButton6=findViewById(R.id.imageButton6)
+        imageButton7=findViewById(R.id.imageButton7)
+        imageButton8=findViewById(R.id.imageButton8)
 
+        buttons = listOf(imageButton1,imageButton2,imageButton3,imageButton4,imageButton5,imageButton6,imageButton7,imageButton8)
+
+        val images = mutableListOf(R.drawable.icon_1, R.drawable.icon_2, R.drawable.icon_3, R.drawable.icon_4)
+        // Duplicate the current images
+        images.addAll(images)
+        // Mixed the order of images
+        images.shuffle()
+
+        viewModel.cards = buttons.indices.map { index ->
+            MemoryCard(images[index])
+        }
+
+        buttons.forEachIndexed { index, button ->
+            button.setOnClickListener {
+                Log.i(TAG, "button clicked!!${index.toString()}")
+
+                // Update Memory Card models
+                viewModel.updateModels(index)
+                // Update the view
+                updateViews()
+            }
+        }
     }
 
-
+    private fun updateViews() {
+        viewModel.cards.forEachIndexed { index, card ->
+            val button = buttons[index]
+            if (card.isMatched) {
+                button.alpha = 0.1f
+            }
+            button.setImageResource(if (card.isFaceUp) card.identifier else R.drawable.pokemon_ball)
+        }
+    }
 
 }
--- app/src/main/java/tdao/example/project2_memorygame_tiendao/MainViewModel.kt
+++ app/src/main/java/tdao/example/project2_memorygame_tiendao/MainViewModel.kt
@@ -1,10 +1,15 @@
 package tdao.example.project2_memorygame_tiendao
 
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
 
 //ViewModel can be reused in many activites
 class MainViewModel: ViewModel() {
+
+    lateinit var cards: List<MemoryCard>
+    private var indexOfSingleSelectedCard: Int? = null
+
     // lifecycle
     init {
         Log.i("MainViewModel", "init")
@@ -13,5 +18,48 @@ class MainViewModel: ViewModel() {
     override fun onCleared() {
         super.onCleared()
         Log.i("MainViewModel", "onCleared")
+    }
+
+    fun updateModels(position: Int) {
+        val card = cards[position]
+
+        //Error checking:
+        if( card.isFaceUp) {
+//            Toast.makeText(this, "Invalid move!", Toast.LENGTH_SHORT).show()
+            return
+        }
+        // Three cases
+        // 0 cards previously flipped over => restore cards + flip over the selected card
+        // 1 card previously flipped over => flip over the selected card + check if the images match
+        // 2 cards previously flipped over => restore cards + flip over the selected card
+        if (indexOfSingleSelectedCard == null) {
+            // 0 or 2 selected cards previously
+            restoreCards()
+            indexOfSingleSelectedCard = position
+        } else {
+            // exactly 1 card was selected previously
+            checkForMatch(indexOfSingleSelectedCard!!, position)
+            indexOfSingleSelectedCard = null
+        }
+        card.isFaceUp = !card.isFaceUp
+    }
+
+
+
+    public fun restoreCards() {
+        for (card in cards) {
+            if (!card.isMatched) {
+                card.isFaceUp = false
+            }
+        }
+    }
+
+    public fun checkForMatch(position1: Int, position2: Int) : Boolean {
+        if (cards[position1].identifier == cards[position2].identifier) {
+//            Toast.makeText(this, "Match found!!", Toast.LENGTH_SHORT).show()
+            cards[position1].isMatched = true
+            cards[position2].isMatched = true
+        }
+        return true
     }
 }
