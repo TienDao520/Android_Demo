7d2d4e9cddb820f3d26574acc11c335bd9aacab9
Tien Dao <tiendao5240@gmail.com>
2022-04-08 20:23:02 -0400

    first commit Project2_MemoryGame_TienDao

---
+++ app/src/main/java/tdao/example/project2_memorygame_tiendao/MainActivity.kt
@@ -0,0 +1,11 @@
+package tdao.example.project2_memorygame_tiendao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
