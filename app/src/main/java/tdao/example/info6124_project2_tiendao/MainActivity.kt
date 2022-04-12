f6306939361bf57c2ca6c516bdf54bef95a924a4
Tien Dao <tiendao5240@gmail.com>
2022-04-12 16:00:36 -0400

    Add SMS permission in Manifest file

--- app/src/main/java/tdao/example/info6124_project2_tiendao/MainActivity.kt
+++ app/src/main/java/tdao/example/info6124_project2_tiendao/MainActivity.kt
@@ -1,24 +1,37 @@
 package tdao.example.info6124_project2_tiendao
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.FragmentContainer
+import androidx.fragment.app.FragmentContainerView
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks {
 
     //Setup Google API Client
     private var mGoogleApiClient: GoogleApiClient? = null
     private val TAG = "MainActivity"
+
+    //For Email
+    private lateinit var textEmailAddress:TextView
+    private lateinit var textEmailSubject:TextView
+    private lateinit var textEmailMessage:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
 
 //        binding = ActivityMapsBinding.inflate(layoutInflater)
 //        setContentView(binding.root)
@@ -28,7 +41,16 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks {
 //            .findFragmentById(R.id.map) as SupportMapFragment
 //        mapFragment.getMapAsync(this)
 
+
     }
+
+    override fun onResume() {
+        super.onResume()
+        // Start Lifecycle of a fragment
+
+    }
+
+
 
     override fun onStart() {
         Log.i(TAG, "onStart ${mGoogleApiClient.toString()}")
@@ -54,6 +76,82 @@ class MainActivity : AppCompatActivity(), GoogleApiClient.ConnectionCallbacks {
         Log.i(TAG, "onConnectionSuspended")
     }
 
-    fun onButtonClick(view: View) {}
+    fun onButtonClick(view: View) {
+        when(view.id) {
+            R.id.mapButton -> {
+                onMapFragment()
+            }
+            R.id.emailButton -> {
+                onEmailFragment()
+            }
+            R.id.smsButton -> {
+                onSmsFragment()
+            }
+        }
+    }
+
+    private fun onSmsFragment() {
+        try {
+            val fragment: SmsFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as SmsFragment
+        } catch (e:Exception) {
+            deleteCurrentFragment()
+            val fragment = SmsFragment()
+            supportFragmentManager.inTransaction { add(R.id.fragmentContainerView, fragment)}
+        }
+    }
+
+    private fun onEmailFragment() {
+        try {
+            val fragment: EmailFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as EmailFragment
+        } catch (e:Exception) {
+            deleteCurrentFragment()
+            val fragment = EmailFragment()
+            supportFragmentManager.inTransaction { add(R.id.fragmentContainerView, fragment)}
+        }
+    }
+
+    private fun onMapFragment() {
+        try {
+            val fragment: MapsFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as MapsFragment
+        } catch (e:Exception) {
+            deleteCurrentFragment()
+            val fragment = MapsFragment()
+            //            val fragmentTransaction = supportFragmentManager.beginTransaction()
+//            fragmentTransaction.add(R.id.fragmentContainerView, fragment)
+//            fragmentTransaction.commit()
+            supportFragmentManager.inTransaction { add(R.id.fragmentContainerView, fragment)}
+        }
+    }
+
+    inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
+        val fragmentTransaction = beginTransaction()
+        fragmentTransaction.func()
+        fragmentTransaction.commit()
+    }
+
+    private fun deleteCurrentFragment() {
+        val currentFragmentContainer = findViewById<FragmentContainerView>(R.id.fragmentContainerView)
+        currentFragmentContainer.removeAllViews()
+    }
+
+    fun onEmailClick(view: View) {
+
+        textEmailAddress = findViewById(R.id.textEmailAddress)
+        textEmailSubject = findViewById(R.id.textEmailSubject)
+        textEmailMessage = findViewById(R.id.textEmailMessage)
+        val toAddress = arrayOf("${textEmailAddress.text}")
+        composeEmail(toAddress, "${textEmailSubject.text}", "${textEmailMessage.text}")
+    }
+
+    private fun composeEmail(addresses: Array<String>, subject: String, body: String) {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:") // only email apps should handle this
+            putExtra(Intent.EXTRA_EMAIL, addresses)
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
 
 }
