c9af74b9e12c639cf7f94d717d0feb2ac16b426f
Tien Dao <tiendao5240@gmail.com>
2022-04-11 20:59:16 -0400

    CLean code: done

--- app/src/main/java/tdao/example/project2_memorygame_tiendao/MainActivity.kt
+++ app/src/main/java/tdao/example/project2_memorygame_tiendao/MainActivity.kt
@@ -11,15 +11,6 @@ import tdao.example.project2_memorygame_tiendao.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
-    private lateinit var buttons: List<ImageButton>
-    private lateinit var imageButton1: ImageButton
-    private lateinit var imageButton2: ImageButton
-    private lateinit var imageButton3: ImageButton
-    private lateinit var imageButton4: ImageButton
-    private lateinit var imageButton5: ImageButton
-    private lateinit var imageButton6: ImageButton
-    private lateinit var imageButton7: ImageButton
-    private lateinit var imageButton8: ImageButton
 
     private val TAG = "MainActivity"
 
@@ -28,7 +19,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
 
         //DataBinding:Step3: add binding
         binding =
@@ -41,73 +31,6 @@ class MainActivity : AppCompatActivity() {
         binding.viewModel = viewModel
         binding.executePendingBindings()
         binding.lifecycleOwner = this
-
-//        imageButton1=findViewById(R.id.imageButton1)
-//        imageButton2=findViewById(R.id.imageButton2)
-//        imageButton3=findViewById(R.id.imageButton3)
-//        imageButton4=findViewById(R.id.imageButton4)
-//        imageButton5=findViewById(R.id.imageButton5)
-//        imageButton6=findViewById(R.id.imageButton6)
-//        imageButton7=findViewById(R.id.imageButton7)
-//        imageButton8=findViewById(R.id.imageButton8)
-//
-//        buttons = listOf(imageButton1,imageButton2,imageButton3,imageButton4,imageButton5,imageButton6,imageButton7,imageButton8)
-
-
-
-
-//        val images = mutableListOf(R.drawable.icon_1, R.drawable.icon_2, R.drawable.icon_3, R.drawable.icon_4)
-//        // Duplicate the current images
-//        images.addAll(images)
-//        // Mixed the order of images
-//        images.shuffle()
-//
-//        viewModel.cards = images.indices.map { index ->
-//            MemoryCard(images[index])
-//        }
-
-//        Log.i("AAA",viewModel.cards.toString() )
-
-//        viewModel.cards = images.forEachIndexed{index, image ->
-//
-//        }
-
-//        buttons.forEachIndexed { index, button ->
-//            button.setOnClickListener {
-//                Log.i(TAG, "button clicked!!${index.toString()}")
-//
-//                // Update Memory Card models
-////                viewModel.updateModels(index)
-//                // Update the view
-//                updateViews()
-//            }
-//        }
-
-
-//        Log.i("MainViewModel", "init")
-//        val images = mutableListOf(R.drawable.icon_1, R.drawable.icon_2, R.drawable.icon_3, R.drawable.icon_4)
-//        images.addAll(images)
-//        // Mixed the order of images
-//        images.shuffle()
-//
-////        val images_ = mutableListOf(R.drawable.icon_1, R.drawable.icon_2, R.drawable.icon_3, R.drawable.icon_4)
-//
-//        viewModel._cards.value = images.indices.map { index ->
-//            MemoryCard(images[index])
-//        }
-
     }
-
-//    private fun updateViews() {
-//        viewModel.cards.forEachIndexed { index, card ->
-//            val button = buttons[index]
-//            if (card.isMatched) {
-//                button.alpha = 0.6f
-//            }
-//            button.setImageResource(if (card.isFaceUp) card.identifier else R.drawable.pokemon_ball)
-//        }
-//    }
-
-
 
 }
--- app/src/main/java/tdao/example/project2_memorygame_tiendao/MainViewModel.kt
+++ app/src/main/java/tdao/example/project2_memorygame_tiendao/MainViewModel.kt
@@ -11,30 +11,29 @@ import com.google.android.material.internal.ContextUtils.getActivity
 class MainViewModel: ViewModel() {
 
     private val _cards = MutableLiveData<List<MemoryCard>>()
-
     val cards: LiveData<List<MemoryCard>> = _cards
+
+    private val count_ = MutableLiveData(0)
+    val count: LiveData<Int> = count_
+
     private var indexOfSingleSelectedCard: Int? = null
-    var imgDefault =R.drawable.pokemon_ball
+    var imgDefault =R.drawable.pokemon_flag
 
 
     // Duplicate the current images
-
+    val images = mutableListOf(R.drawable.icon_1, R.drawable.icon_2, R.drawable.icon_3, R.drawable.icon_4)
 
     // lifecycle
     init {
         Log.i("MainViewModel", "init")
-        val images = mutableListOf(R.drawable.icon_1, R.drawable.icon_2, R.drawable.icon_3, R.drawable.icon_4)
         images.addAll(images)
         // Mixed the order of images
         images.shuffle()
-
-//        val images_ = mutableListOf(R.drawable.icon_1, R.drawable.icon_2, R.drawable.icon_3, R.drawable.icon_4)
 
         _cards.value = images.indices.map { index ->
             MemoryCard(images[index])
         }
         Log.i("cards_.value", _cards.value.toString())
-//        _cards.value = ArrayList()
     }
 
     override fun onCleared() {
@@ -65,24 +64,18 @@ class MainViewModel: ViewModel() {
         }
         card?.isFaceUp  = !card?.isFaceUp!!
 
-        //
-//        cards.forEachIndexed { index, card ->
-//            Log.i("MainViewModel", card.toString())
-//            Log.i("MainViewModelIndex", index.toString())
-//            button.setImageResource(if (card.isFaceUp) card.identifier else R.drawable.pokemon_ball)
-//        }
-
-
-
         Log.i("MainViewModel", card.toString())
         cards.value?.get(0)?.identifier.let { Log.i("MainViewModelcards", it.toString()) }
         cards.value?.get(0)?.isFaceUp.let { Log.i("MainViewModelcards_isFaceUp", it.toString()) }
         _cards.value = _cards.value
+
+        count_.value = (count_.value ?: 0)+1
+        Log.i("ViewModel", "count " +count.toString())
     }
 
 
 
-    public fun restoreCards() {
+    fun restoreCards() {
         for (card in _cards.value!!) {
             if (!card.isMatched) {
                 card.isFaceUp = false
@@ -90,7 +83,7 @@ class MainViewModel: ViewModel() {
         }
     }
 
-    public fun checkForMatch(position1: Int, position2: Int) : Boolean {
+    fun checkForMatch(position1: Int, position2: Int) : Boolean {
         if (_cards.value?.get(position1)?.identifier  == _cards.value?.get(position2)?.identifier) {
 //            Toast.makeText(getPranentActivity(MainActivity.class), "Match found!!", Toast.LENGTH_SHORT).show()
             _cards.value?.get(position1)?.isMatched = true
@@ -98,4 +91,17 @@ class MainViewModel: ViewModel() {
         }
         return true
     }
+
+    fun resetCards() {
+
+        images.shuffle()
+
+        _cards.value = images.indices.map { index ->
+            MemoryCard(images[index])
+        }
+        count_.value =0;
+
+    }
+
+
 }
