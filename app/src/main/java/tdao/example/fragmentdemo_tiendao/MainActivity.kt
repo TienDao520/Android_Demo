2f11a18942a7faed86f54ea266193d3b75c529b0
Tien Dao <tiendao5240@gmail.com>
2022-04-11 20:55:23 -0400

    Fragment: done

--- app/src/main/java/tdao/example/fragmentdemo_tiendao/MainActivity.kt
+++ app/src/main/java/tdao/example/fragmentdemo_tiendao/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
@@ -52,11 +54,44 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun onButtonAdd(view: View) {}
-    fun onButtonRemove(view: View) {}
+
     fun onButtonGo(view: View) {
         val intent = Intent(this,SecondActivity::class.java)
         startActivity(intent)
     }
 
+    inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
+        val fragmentTransaction = beginTransaction()
+        fragmentTransaction.func()
+        fragmentTransaction.commit()
+    }
+
+    fun onButtonAdd(view: View) {
+        try {
+            val fragment: FragmentNavigate =
+                supportFragmentManager.findFragmentById(R.id.fragmentContainerViewNavigateMain) as FragmentNavigate
+            Toast.makeText(this,getString(R.string.addfrag)+" Error", Toast.LENGTH_LONG).show()
+        }
+        catch(e:Exception){
+            // create a new instance of the fragment
+            val fragment = FragmentNavigate()
+            //add the fragment to the fragment container, using inline function
+            //supportFragmentManager.inTransaction { add(R.id.fragmentContainerViewNavigateMain, fragment) }
+            //val fragmentTransaction = supportFragmentManager.beginTransaction()
+            val fragmentTransaction = getSupportFragmentManager().beginTransaction()
+            fragmentTransaction.add(R.id.fragmentContainerViewNavigateMain, fragment)
+            fragmentTransaction.commit()
+        }
+    }
+    fun onButtonRemove(view: View) {
+        try {
+            val fragment: FragmentNavigate =
+                supportFragmentManager.findFragmentById(R.id.fragmentContainerViewNavigateMain) as FragmentNavigate
+            supportFragmentManager.inTransaction { remove(fragment) }
+        }
+        catch(e:Exception){
+            Toast.makeText(this,getString(R.string.removefrag)+" Error",Toast.LENGTH_LONG).show()
+
+        }
+    }
 }
