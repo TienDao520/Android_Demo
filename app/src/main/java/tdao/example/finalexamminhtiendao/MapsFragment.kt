e9f6610a469797ead39a1b5b8f6540098bccc3b1
Tien Dao <tiendao5240@gmail.com>
2022-04-18 11:33:46 -0400

    Added the current address

--- app/src/main/java/tdao/example/finalexamminhtiendao/MapsFragment.kt
+++ app/src/main/java/tdao/example/finalexamminhtiendao/MapsFragment.kt
@@ -1,6 +1,8 @@
 package tdao.example.finalexamminhtiendao
 
 import android.Manifest
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
 import androidx.fragment.app.Fragment
 
@@ -20,6 +22,8 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.io.IOException
+import java.util.*
 
 class MapsFragment : Fragment() , OnMapReadyCallback {
 
@@ -108,5 +112,42 @@ class MapsFragment : Fragment() , OnMapReadyCallback {
 
                 }
             }
+    }
+
+    /* uses reverse geocoding to determine an address from
+    LatLong position.
+    Use Geocoding to determine the LatLong position from address
+    In this case we are giving the LatLong of Bud Gardens */
+    private fun getAddress(loc:LatLng): String? {
+        //Geocoder transfer from location to latlng value
+        val geocoder = Geocoder(requireActivity(), Locale.getDefault())
+        var addresses: List<Address>? = null
+        try {
+            //If you want to get more values you can change the maxResult value
+            addresses = geocoder.getFromLocation(loc!!.latitude, loc!!.longitude, 1)
+        } catch (e1: IOException) {
+            Log.e(TAG, getString(R.string.service), e1)
+        } catch (e2: IllegalArgumentException) {
+            Log.e(TAG, getString(R.string.invalid_lat_long)+ ". " +
+                    "Latitude = " + loc!!.latitude +
+                    ", Longitude = " +
+                    loc!!.longitude, e2)
+        }
+        // If the reverse geocode returned an address
+        if (addresses != null) {
+            // Get the first address
+            val address = addresses[0]
+            val addressText = String.format(
+                "%s, %s, %s",
+                address.getAddressLine(0), // If there's a street address, add it
+                address.locality,                 // Locality is usually a city
+                address.countryName)              // The country of the address
+            return addressText
+        }
+        else
+        {
+            Log.e(TAG, getString(R.string.no_address_found))
+            return ""
+        }
     }
 }
