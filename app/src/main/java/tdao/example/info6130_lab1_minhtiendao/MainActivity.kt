2b54b9d26bbac7d073fe8670ebde4910e4a3d1f2
Tien Dao <tiendao5240@gmail.com>
2022-05-13 22:01:49 -0400

    First commit INFO6130_Lab1_MinhTienDao

---
+++ app/src/main/java/tdao/example/info6130_lab1_minhtiendao/MainActivity.kt
@@ -0,0 +1,11 @@
+package tdao.example.info6130_lab1_minhtiendao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
