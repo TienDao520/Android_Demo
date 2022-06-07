8bbdf4752cf3cc614b6ea3a2f7a5b809e302c021
Tien Dao <tiendao5240@gmail.com>
2022-06-06 20:46:06 -0400

    MainActivity: declare variables

--- app/src/main/java/tdao/example/viewanimationexample_kt/MainActivity.kt
+++ app/src/main/java/tdao/example/viewanimationexample_kt/MainActivity.kt
@@ -2,10 +2,22 @@ package tdao.example.viewanimationexample_kt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    //FadeIn: Step1: declare variables
+    var btnFadeIn : Button? = null
+    var txtFadeIn : TextView? = null
+    var animFadeIn : Animation? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
