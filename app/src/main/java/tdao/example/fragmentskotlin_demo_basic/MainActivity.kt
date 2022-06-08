ad4acc3df54f09fdfbcc9a6c2d532cfc771eb770
Tien Dao <tiendao5240@gmail.com>
2022-06-08 14:34:50 -0400

    FragmentsKotlin_Demo_Basic

---
+++ app/src/main/java/tdao/example/fragmentskotlin_demo_basic/MainActivity.kt
@@ -0,0 +1,11 @@
+package tdao.example.fragmentskotlin_demo_basic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
