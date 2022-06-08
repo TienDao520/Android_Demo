19e6dc22c35a0cbb0dc71fe3add4de9e28fe3530
Tien Dao <tiendao5240@gmail.com>
2022-06-08 18:12:24 -0400

    Added Start wheel spin

--- app/src/main/java/tdao/example/info6130_midterm_minhtiendao/MainActivity.kt
+++ app/src/main/java/tdao/example/info6130_midterm_minhtiendao/MainActivity.kt
@@ -2,10 +2,42 @@ package tdao.example.info6130_midterm_minhtiendao
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    //Rotate: Step1: declare variables
+    var startSpin : Button? = null
+    var stopSpin : Button? = null
+    var speedSpin : Button? = null
+    var animRotate : Animation? = null
+    var imageWheel:ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startSpin=findViewById(R.id.startBtn)
+        stopSpin=findViewById(R.id.stopBtn)
+        speedSpin=findViewById(R.id.speedBtn)
+
+        imageWheel=findViewById(R.id.imageWheel)
+
+        // Added the animation from the anim lib folder
+        animRotate = AnimationUtils.loadAnimation (applicationContext, R.anim.rotate)
+
+
     }
+
+    fun onStartBtn(view: View) {
+        imageWheel?.startAnimation(animRotate)
+    }
+    fun onStopBtn(view: View) {
+        imageWheel?.clearAnimation()
+    }
+    fun onSpeedBtn(view: View) {}
 }
