63dac3a947a9ce7eeb44ba4486c18f99a8aa4f0c
Tien Dao <tiendao5240@gmail.com>
2022-07-07 11:11:57 -0400

    CountDownApp: Done

--- app/src/main/java/tdao/example/multithreadeverycycle_kt/MainActivity.kt
+++ app/src/main/java/tdao/example/multithreadeverycycle_kt/MainActivity.kt
@@ -2,10 +2,45 @@ package tdao.example.multithreadeverycycle_kt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var titleTextView: TextView
+    lateinit var mainHandler: Handler
+    val delayTime = 1000
+    private var secondsLeft = 60
+
+    private val updateTextTask = object: Runnable {
+        override fun run() {
+            minusOneSecond()
+            mainHandler.postDelayed(this, delayTime.toLong())
+        }
+    }
+
+    private fun minusOneSecond() {
+        if (secondsLeft > 0){
+            secondsLeft -=1
+
+            titleTextView.text= secondsLeft.toString()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        titleTextView = findViewById(R.id.titleTextView)
+        mainHandler = Handler(Looper.getMainLooper())
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mainHandler.removeCallbacks(updateTextTask)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mainHandler.post(updateTextTask)
     }
 }
