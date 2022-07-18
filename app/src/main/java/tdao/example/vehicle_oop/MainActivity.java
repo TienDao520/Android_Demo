f421edfbfc32a65ad0b7799550797bb5bee048f6
Tien Dao <tiendao5240@gmail.com>
2022-07-18 14:40:14 -0400

    Created new car (c1) instance

--- app/src/main/java/tdao/example/vehicle_oop/MainActivity.java
+++ app/src/main/java/tdao/example/vehicle_oop/MainActivity.java
@@ -9,10 +9,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tvInfo1,tvInfo2,tvInfo3,tvInfo4,tvInfo5;
+    TextView tvInfo1, tvInfo2, tvInfo3, tvInfo4, tvInfo5;
     Vehicle v1;
 
-
+    Car c1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,15 +20,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        tvInfo1= (TextView) findViewById(R.id.tv_info1);
-        tvInfo2= (TextView) findViewById(R.id.tv_info2);
-        tvInfo3= (TextView) findViewById(R.id.tv_info3);
-        tvInfo4= (TextView) findViewById(R.id.tv_info4);
-        tvInfo5= (TextView) findViewById(R.id.tv_info5);
+        tvInfo1 = (TextView) findViewById(R.id.tv_info1);
+        tvInfo2 = (TextView) findViewById(R.id.tv_info2);
+        tvInfo3 = (TextView) findViewById(R.id.tv_info3);
+        tvInfo4 = (TextView) findViewById(R.id.tv_info4);
+        tvInfo5 = (TextView) findViewById(R.id.tv_info5);
 
-        v1 = new Vehicle("Test",25,120,100,4,false);
+        v1 = new Vehicle("Test", 25, 120, 100, 4, false);
+        c1 = new Car("TestCar",30, 160, 120,4,false,"V2");
 
         tvInfo1.setText(v1.toString());
+        tvInfo2.setText(c1.toString());
         tvInfo5.setText(v1.sound());
 
     }
