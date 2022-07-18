366a54b044070db44cde827cfe5d065506b2287c
Tien Dao <tiendao5240@gmail.com>
2022-07-18 14:51:05 -0400

    Created new instance

--- app/src/main/java/tdao/example/vehicle_oop/MainActivity.java
+++ app/src/main/java/tdao/example/vehicle_oop/MainActivity.java
@@ -11,8 +11,9 @@ public class MainActivity extends AppCompatActivity {
 
     TextView tvInfo1, tvInfo2, tvInfo3, tvInfo4, tvInfo5;
     Vehicle v1;
-
     Car c1;
+    Mercedes c200;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +29,11 @@ public class MainActivity extends AppCompatActivity {
 
         v1 = new Vehicle("Test", 25, 120, 100, 4, false);
         c1 = new Car("TestCar",30, 160, 120,4,false,"V2");
+        c200 = new Mercedes("C200", 70,240,200,4,true, "v8", "Black & Red", "LED", "Black");
 
         tvInfo1.setText(v1.toString());
         tvInfo2.setText(c1.toString());
+        tvInfo3.setText(c200.toString());
         tvInfo5.setText(v1.sound());
 
     }
