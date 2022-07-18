3adc2fdb5d15147a1c319103a21f33cf26406ef4
Tien Dao <tiendao5240@gmail.com>
2022-07-18 14:43:18 -0400

    Car class override toString from Vehicle - using super keyword

--- app/src/main/java/tdao/example/vehicle_oop/Car.java
+++ app/src/main/java/tdao/example/vehicle_oop/Car.java
@@ -1,5 +1,7 @@
 package tdao.example.vehicle_oop;
 
+import androidx.annotation.NonNull;
+
 public class Car extends Vehicle{
 
     private final String engineType;
@@ -7,5 +9,15 @@ public class Car extends Vehicle{
     public Car(String name, int speed, int maxSpeed, int maxFuelTank, int numberOfWheels, boolean hasAdvanceBrakeSystem, String engineType) {
         super(name, speed, maxSpeed, maxFuelTank, numberOfWheels, hasAdvanceBrakeSystem);
         this.engineType = engineType;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return String.format("%s \n%s %s", super.toString(), "Engine: ", getEngineType());
     }
 }
