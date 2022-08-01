722aa955598f8ea9a5a716cd3111d38bbd2b2213
Tien Dao <tiendao5240@gmail.com>
2022-08-01 18:05:06 -0400

    GameView: Get a reference to the SurfaceView object's SurfaceHolder - control and monitor Surface

--- app/src/main/java/tdao/example/surfaceviewdemo_kt/GameView.kt
+++ app/src/main/java/tdao/example/surfaceviewdemo_kt/GameView.kt
@@ -1,14 +1,28 @@
 package tdao.example.surfaceviewdemo_kt
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
+import android.view.SurfaceHolder
 import android.view.SurfaceView
 
 class GameView @JvmOverloads constructor(
-    context: Context, attrs: AttributeSet? = null
-) : SurfaceView(context, attrs), Runnable {
+    private val mContext: Context,
+    attrs: AttributeSet? = null
+) : SurfaceView(mContext, attrs), Runnable {
+
+    private val mSurfaceHolder: SurfaceHolder
+
     override fun run() {
         TODO("Not yet implemented")
     }
+
+    init {
+        mSurfaceHolder = holder //from getHolder()
+    }
+
+
 
 }
