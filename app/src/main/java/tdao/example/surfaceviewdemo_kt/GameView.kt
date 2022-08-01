fc3a524073758621e2bf4efc6ef75a76ffcc25b3
Tien Dao <tiendao5240@gmail.com>
2022-08-01 17:56:58 -0400

    GameView: Added @JvmOverloads annotation for input parameters of contructors

---
+++ app/src/main/java/tdao/example/surfaceviewdemo_kt/GameView.kt
@@ -0,0 +1,11 @@
+package tdao.example.surfaceviewdemo_kt
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.SurfaceView
+
+class GameView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : SurfaceView(context, attrs) {
+
+}
